9b40017f5e0a4f567aba2d515bd7b85657d24f25
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2015-10-15 21:04:31 +0000

    test multiple numbers return sum

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,8 +14,19 @@ public class CalculatorTest {
 		assertEquals(0, Calculator.add(""));
 	}
 
-
-	public void testOneNumber(){
-    assertEquals(1, Calculator.add("1"));
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
 }
