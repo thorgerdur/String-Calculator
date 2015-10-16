2865d0d677b2ee84e96b54c60d0b01b9a3ebb282
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2015-10-16 12:41:22 +0000

    aftur fra byrjun i fyrsta testi

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,49 +5,6 @@ import java.util.stream.Stream;*/
 public class Calculator {
 
 		public static int add(String text){
-
-	/*	String delimiter = ",|\n";*/
-
-		if(text.equals("")){
 			return 0;
 		}
-		/*if(text.startsWith("//"))
-		{
-			String [] number = text.split("\n",2);
-			String delimiter = number[0].substring(2);
-			text = number[1];
-		}*/
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}
-		else if(text.contains("\n")){
-			return sum(splitNumbersLine(text));
-		}
-		else
-			return 1;
-	}
-
-	private static int toInt(String number){
-		return Integer.parseInt(number);
-	}
-
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
-	}
-	private static String[] splitNumbersLine(String numbers){
-		return numbers.split("\n");
-	}
-
-  private static int sum(String[] numbers){
- 	  int total = 0;
-      for(String number : numbers){
-				if(toInt(number)>1000)
-				{
-					continue;
-				}
-				total += toInt(number);
-		}
-		return total;
-  }
-
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,49 +14,13 @@ public class CalculatorTest {
     }
 
 	@Test
-	public void testEmptyString() throws Exception  {
+	public void testEmptyString(){
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() throws Exception  {
+	public void testOneNumber(){
 		assertEquals(1, Calculator.add("1"));
 	}
-
-	@Test
-	public void testTwoNumbers()  throws Exception {
-		assertEquals(3, Calculator.add("1,2"));
-	}
-
-	@Test
-    public void testMultipleNumbers() throws Exception {
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
-	@Test
-		public void testNewLine() throws Exception {
-			assertEquals(3, Calculator.add("1\n2"));
-		}
-
-	@Test
-		public void testNewLineMultipleNumbers() throws Exception {
-			assertEquals(6, Calculator.add("1\n2\n3"));
-		}
-
-	@Test
-		public void testNewLineOrComma() throws Exception {
-			assertEquals(6, Calculator.add("1,2\n3"));
-		}
-
-	@Test
-		public void testNewDelimiter() throws Exception {
-			assertEquals(3, Calculator.add("//;\n1;2"));
-		}
-
-	@Test
-		public void testNotGreaterthan1000() throws Exception {
-			assertEquals(20, Calculator.add("10,10,1001"));
-		}
-
 
 }
