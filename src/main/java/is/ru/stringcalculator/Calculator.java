549ea103dada4dbf0620bd498cba037752ff091d
Thorgerdur Eiriksdottir <thorgerdur14@ru.is>
2015-10-16 13:33:34 +0000

    summa med 2 tolum og kommu a milli virkar

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -8,7 +8,32 @@ public class Calculator {
 			{
 				return 0;
 			}
+			else if(text.length() == 1)
+			{
+				return toInt(text);
+			}
+			else if(text.contains(","))
+			{
+				return sum(splitNumbers(text));
+			}
 			else
 				return 1;
+
 		}
+
+		private static int toInt(String number){
+			return Integer.parseInt(number);
+		}
+
+		private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+   	}
+
+		private static int sum(String[] numbers){
+ 	  int total = 0;
+      for(String number : numbers){
+				total += toInt(number);
+		}
+		return total;
+  }
 }
